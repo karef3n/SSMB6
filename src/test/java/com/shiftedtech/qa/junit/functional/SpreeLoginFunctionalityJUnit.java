0a940935400118f75a734817b9ca0d100ee58b6f
karef3n <karef100n@gmail.com>
2018-11-11 12:00:48 -0500

    created

---
+++ src/test/java/com/shiftedtech/qa/junit/functional/SpreeLoginFunctionalityJUnit.java
@@ -0,0 +1,22 @@
+package com.shiftedtech.qa.junit.functional;
+
+import com.shiftedtech.qa.framework.scriptbase.SpreeScriptBaseJUnit;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ * Created by ShiftTeacher on 2/3/2018.
+ */
+@RunWith(Parameterized.class)
+public class SpreeLoginFunctionalityJUnit extends SpreeScriptBaseJUnit{
+
+    @Test
+    public void verifySuccessfullLogin(){
+        homePage.verifyPageTitle("Spree Demo Site");
+        homePage.navigateToLoginPage();
+        loginPage.verifyPageTitle("Login - Spree Demo Site");
+        loginPage.login("shiftqa01@gmail.com", "shiftedtech");
+        homePage.verifyLoginSuccess();
+    }
+}
